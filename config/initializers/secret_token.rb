7838a81c2fc3ead0977d10727693675edb521437
Vagios Vlachos <bagios234@gmail.com>
2014-09-09 17:05:04 +0300

    Initial commit. Fresh rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NestedCommentsRails::Application.config.secret_key_base = '0acceb1f8445c2f84847666c6ecd561e98c7810c90961e5f1867cb40b91fa6f53206de9887edcbf7d54243474d890a96189c43eb876a1b9fd832771bf27dcbeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NestedCommentsRails::Application.config.session_store :cookie_store, key: '_nested_comments_rails_session'
